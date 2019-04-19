6c59195d2c7d9455e1a822c4bc062f41f93059c8
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-19 19:50:32 +0700

    Initial wish migration

--- wishbook/models.py
+++ wishbook/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Wish(models.Model):
+    wish_text = models.TextField()
+    wish_owner = models.CharField(max_length=50)
+    shown = models.BooleanField()

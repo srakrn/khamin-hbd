8488a08739d540089e98de0f4702c688bbbd716d
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-19 19:57:25 +0700

    Prettify admin display

--- wishbook/admin.py
+++ wishbook/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Wish
 
 # Register your models here.
+
+
+class WishAdmin(admin.ModelAdmin):
+    list_display = ("wish_text", "wish_owner")
+
+
+admin.site.register(Wish, WishAdmin)

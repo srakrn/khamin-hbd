3ba1a7867cacdc537ce497a2794b88c3de66258c
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-19 20:12:55 +0700

    Fetch wishes

--- wishbook/views.py
+++ wishbook/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Wish
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "pages/index.html")
+    all_wishes = Wish.objects.order_by("-created").all()
+    return render(request, "pages/index.html", {"all_wishes": all_wishes})

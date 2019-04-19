cf8b8334976fbe0c10c9b486ebe597f2508c509c
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-19 19:58:09 +0700

    Models and migrations

--- wishbook/models.py
+++ wishbook/models.py
@@ -7,3 +7,7 @@ class Wish(models.Model):
     wish_text = models.TextField()
     wish_owner = models.CharField(max_length=50)
     shown = models.BooleanField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} from {}".format(self.wish_text[:min(len(self.wish_text), 10)], self.wish_owner)

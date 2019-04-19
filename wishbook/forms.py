cb3a6b1601b17cb8176ef913e256ff46c525753d
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-19 22:44:44 +0700

    Loading reCAPTCHA into forms

--- wishbook/forms.py
+++ wishbook/forms.py
@@ -1,8 +1,11 @@
 from django.forms import ModelForm
 from .models import Wish
+from captcha.fields import ReCaptchaField
 
 
 class WishForm(ModelForm):
+    captcha = ReCaptchaField(label="กรุณายืนยันว่าคุณไม่ใช่บอท")
+
     class Meta:
         model = Wish
         fields = ["wish_text", "wish_owner"]

dd9281ddfba4cd50bf97706b52db5aff8738b455
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-19 20:18:10 +0700

    Changing fields' names, Setting default shown to True

--- wishbook/models.py
+++ wishbook/models.py
@@ -6,8 +6,10 @@ from django.db import models
 class Wish(models.Model):
     wish_text = models.TextField()
     wish_owner = models.CharField(max_length=50)
-    shown = models.BooleanField()
+    shown = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return "{} from {}".format(self.wish_text[:min(len(self.wish_text), 10)], self.wish_owner)
+        return "{} from {}".format(
+            self.wish_text[: min(len(self.wish_text), 10)], self.wish_owner
+        )

4d7814418475e93e69533cb8d992a26166f50037
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-19 22:53:15 +0700

    Customising error texts

--- wishbook/forms.py
+++ wishbook/forms.py
@@ -4,8 +4,16 @@ from captcha.fields import ReCaptchaField
 
 
 class WishForm(ModelForm):
-    captcha = ReCaptchaField(label="กรุณายืนยันว่าคุณไม่ใช่บอท")
+    captcha = ReCaptchaField(
+        label="กรุณายืนยันว่าคุณไม่ใช่บอท",
+        error_messages={"required": "กรุณายืนยันว่าไม่ใช่บอท"},
+    )
 
     class Meta:
         model = Wish
         fields = ["wish_text", "wish_owner"]
+        error_messages = {
+            "wish_text": {"required": "กรุณาระบุข้อความอวยพร"},
+            "wish_owner": {"required": "กรุณาระบุชื่อ"},
+        }
+
